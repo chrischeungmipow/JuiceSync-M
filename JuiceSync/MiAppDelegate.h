39d076ab5d0cdb8fa7f364de0510fbf9734c4f18
Guorong ZHANG <guorongzhang@Guorongs-Mac-mini.local>
2014-04-08 09:24:59 +0800

    Initial Commit

---
+++ JuiceSync/MiAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MiAppDelegate.h
+//  JuiceSync
+//
+//  Created by Guorong ZHANG on 4/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JuiceSync/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JuiceSync
+//
+//  Created by Guorong ZHANG on 4/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MiAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MiAppDelegate class]));
+    }
+}
